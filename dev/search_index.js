20a39bf54407db2c448b99a36147c98cc1078080
Documenter.jl <documenter@juliadocs.github.io>
2022-10-17 18:57:02 +0000

    build based on 11001d3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GGplot","category":"page"},{"location":"#GGplot","page":"Home","title":"GGplot","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GGplot.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GGplot]","category":"page"}]
+}
