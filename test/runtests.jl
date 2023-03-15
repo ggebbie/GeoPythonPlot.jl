2a93102c223d8f645be74e9768a1874c728c3b49
G Jake Gebbie <ggebbie@whoi.edu>
2023-03-15 08:10:34 -0400

    add a plotextent runtest

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,11 @@ using GGplot
 using Test
 
 @testset "GGplot.jl" begin
+
     # Write your tests here.
+
+    # will this plot run?
+    latbox = [50,60]; lonbox = [30,40]
+    plotextent(latbox,lonbox)
+
 end
