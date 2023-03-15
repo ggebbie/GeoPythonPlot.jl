9cadf392a83b46d1f7517f3e1fa33325db2266b1
G Jake Gebbie <ggebbie@whoi.edu>
2023-03-15 08:48:58 -0400

    rm manifest, add build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -1,7 +1,13 @@
 using Pkg, Conda
 
+println("building GGplot")
+println("adding packages not in general Julia registry")
+Pkg.add(url="https://github.com/ggebbie/TMI.jl")
+Pkg.add(url="https://github.com/tejasvaidhyadev/GoogleDrive.jl")
+
 if lowercase(get(ENV, "CI", "false")) == "true"    
 
+    println("inside CI loop")
     ENV["PYTHON"] = ""
     Pkg.build("PyCall")
 
