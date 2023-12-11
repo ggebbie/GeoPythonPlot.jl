4a86b09ab07346c7b5f170f75df565caed4dccf5
G Jake Gebbie <ggebbie@whoi.edu>
2023-12-11 12:32:26 -0500

    clean up comments in runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,36 +1,25 @@
 using GeoPythonPlot
-#using PythonCall
-#using PythonPlot
-#using TMI
 using Test
 
 pyversion = GeoPythonPlot.pyconvert(String, GeoPythonPlot.pyimport("sys").version)
 @info("PythonPlot is using Matplotlib $(GeoPythonPlot.PythonPlot.version) with Python $pyversion")
-
-# get test field
-#TMIversion = "modern_90x45x33_GH10_GH12"
-#A, Alu, γ, TMIfile, L, B = config_from_nc(TMIversion,compute_lu = false);
-
-# get observations at surface
-# set them as surface boundary condition
-#testfield = readfield(TMIfile,"PO₄",γ)
-Nx = 90
-Ny = 45
-testfield = randn(Nx+1,Ny+1)
-lon = range(0.0,360.0,length=Nx+1)
-lat = range(-90.0,90.0,length=Ny+1)
 
 @testset "GeoPythonPlot.jl" begin
 
     # Write your tests here.
     @testset "box" begin
         # will this plot run?
-        #pygui(true) # to see the plot
+        #GeoPythonPlot.pygui(true) # to see the plot
         latbox = [50,60]; lonbox = [30,40]
         plotbox(latbox,lonbox)
     end
 
     @testset "planview" begin
+        Nx = 90
+        Ny = 45
+        testfield = randn(Nx+1,Ny+1)
+        lon = range(0.0,360.0,length=Nx+1)
+        lat = range(-90.0,90.0,length=Ny+1)
 
         cntrs = -2:0.5:2
         depth = 1000.0
