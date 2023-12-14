94ee95edee4fa1f7d346d4026b57d79d880a50af
G Jake Gebbie <ggebbie@whoi.edu>
2023-12-14 13:52:49 -0500

    rm unnecessary code

--- src/GeoPythonPlot.jl
+++ src/GeoPythonPlot.jl
@@ -2,9 +2,7 @@ module GeoPythonPlot
 
 using PythonCall
 using PythonPlot
-#using TMI
 
-# Write your package code here.
 export planview, planviewplot,
     section, sectionplot,
     plotbox, tracerinit
@@ -22,36 +20,25 @@ const mpl = pyimport("matplotlib") #PyNULL()
 #Initialize all Python packages - install with conda through Julia
  function __init__()
 
-     #copy!(mpl, pyimport_conda("matplotlib", "matplotlib", "conda-forge"))
      PythonCall.pycopy!(mpl,pyimport("matplotlib"))
-
-     #copy!(cartopy, pyimport_conda("cartopy", "cartopy", "conda-forge"))
      PythonCall.pycopy!(cartopy,pyimport("cartopy"))
-
-
-     #copy!(patch, pyimport_conda("matplotlib.patches", "patches"))
      PythonCall.pycopy!(patch,pyimport("matplotlib.patches"))
-
-     #copy!(ccrs, pyimport_conda("cartopy.crs", "ccrs"))
      PythonCall.pycopy!(ccrs,pyimport("cartopy.crs"))
 
-    # following ClimatePlots.jl
-     #copy!(plt, pyimport_conda("matplotlib.pyplot", "matplotlib", "conda-forge"))
+     # following ClimatePlots.jl
      PythonCall.pycopy!(pyplot,pyimport("matplotlib.pyplot"))
-     
-     #copy!(cmocean, pyimport_conda("cmocean", "cmocean", "conda-forge"))
      PythonCall.pycopy!(cmocean,pyimport("cmocean"))
-     
      println("Python libraries installed")
  end
 
 """
-    function plotbox
-    Generate image showing user-specified ROI
+function `plotbox`
+
+Generate image showing user-specified ROI
+
 # Arguments
 - `latbox`: in format [lat_start, lat_stop]
 - `lonbox`: in format [lon_start, lon_stop]
-
 """
 function plotbox(latbox, lonbox)
     
@@ -91,48 +78,23 @@ function plotbox(latbox, lonbox)
     ax.set_title("User-defined surface patch")
 end
 
-# """
-#     function sectionplot
-#     Plot of section (lat-depth) in ocean
-# # Arguments
-# - `field::Field`, 3d filed of values to be plotted
-# - `lon`: longitude of section
-# - `lims`: contour levels
-# - `titlelabel`: optional title labeln
-# """
-# function sectionplot(field::Field{<:Real}, lon, lims;titlelabel="section plot",fname="figure.png") 
-
-#     Psection = section(field,lon)
-#     cmap_seismic = get_cmap("seismic")
-#     z = field.γ.depth/1000.0
-    
-#     #calc fignum - based on current number of figures
-#     figure()
-#     #cmap_seismic.set_bad(color="black") # doesn't work
-#     contourf(field.γ.lat, z, Psection', lims, cmap=cmap_seismic)
-#     colorbar(orientation="horizontal")
-#     CS = gca().contour(field.γ.lat, z, Psection', lims,colors="k")
-#     gca().clabel(CS, CS.levels, inline=true, fontsize=10)
-#     xlabel("Latitude [°N]")
-#     ylabel("Depth [km]")
-#     gca().set_title(titlelabel)
-#     gca().invert_yaxis()
-#     gca().set_facecolor("black")
-#     savefig(fname)
-# end
-
 """
-    function sectionplotfancy
-    Plot of section (lat-depth) in ocean
+function `sectionplot`
+Plot of meridional section (lat-depth) in ocean
+
 # Arguments
-- `field::Field`, 3d filed of values to be plotted
+- `field::Matrix`, 2D array of values to be plotted
 - `lon`: longitude of section
+- `lat`: vector of latitudes
+- `depth`: vector of depths
 - `lims`: contour levels
+# Optional Arguments
 - `titlelabel`: optional title labeln
+- `fname`: file name
+- `units`: units of 2D array
 """
-function sectionplot(Psection::Matrix, lon, lat, depth, lims;titlelabel="section plot",fname="figure.png") 
+function sectionplot(Psection::Matrix, lon, lat, depth, lims;titlelabel="section plot",fname="figure.png",units=:none) 
 
-    #Psection = section(field,lon)
     cmap_seismic = get_cmap("seismic")
     z = depth/1000.0
     
@@ -146,25 +108,35 @@ function sectionplot(Psection::Matrix, lon, lat, depth, lims;titlelabel="section
     ylabel("Depth [km]")
     gca().set_title(titlelabel)
     gca().invert_yaxis()
-    colorbar(orientation="horizontal")
+    colorbar(orientation="horizontal",label=units)
     gca().set_facecolor("black")
     savefig(fname)
 end
 
 """
-    function planviewplot: from NobleGasRelic
+function `planviewplot`
 
-    formerly planviewplotcartopy
+Plot of plan view (lon-lat) in ocean using cartopy and PythonPlot (matplotlib)
+
+# Arguments
+- `field::Matrix`, 2d array of values to be plotted
+- `lon`: vector of longitudes
+- `lat`: vector of latitudes
+- `depth`: depth of plan view (scalar)
+- `lims`: contour levels
+# Optional Arguments
+- `titlelabel`: optional title label
+- `fname`: output file name
+- `cenlon`: central longitude
+- `units`: units of array `field`
 """
 function planviewplot(cplan::Matrix{<:Real}, lon, lat, depth, lims;titlelabel="planview plot",fname="fname.png",cenlon=-160.0,units=:none) #where T <: Real
 
     cmap_seismic = get_cmap("seismic")
-    #cmap_hot = get_cmap("hot_r")
     cmap_hot = get_cmap("inferno_r")
 
     fig = figure(202)
     clf()
-    #cenlon = -160.0
     proj0 = cartopy.crs.PlateCarree()
     proj = cartopy.crs.PlateCarree(central_longitude=cenlon)
     ax = fig.add_subplot(projection = proj)
@@ -172,9 +144,6 @@ function planviewplot(cplan::Matrix{<:Real}, lon, lat, depth, lims;titlelabel="p
     #ax.coastlines()
     ax.add_feature(cartopy.feature.LAND, zorder=0, edgecolor="black", facecolor="black")
     
-    #outdir = plotsdir()
-    #!isdir(outdir) && mkpath(outdir) 
-    #outfname = plotsdir("vintage.png")
     xlbl = "longitude "*L"[\degree E]"
     ylbl = "latitude "*L"[\degree N]"
     ax.set_title(titlelabel)
@@ -190,67 +159,5 @@ function planviewplot(cplan::Matrix{<:Real}, lon, lat, depth, lims;titlelabel="p
     ax.clabel(CS, CS.levels, inline=true, fontsize=10)
     savefig(fname)
 end
-
-# """
-#     function planviewplot
-#     Plot of plan view (lon-lat) in ocean
-# # Arguments
-# - `field::Field`, 3d filed of values to be plotted
-# - `depth`: depth of plan view
-# - `lims`: contour levels
-# - `titlelabel`: optional title label
-# """
-# function planviewplot(c::Field{T}, depth, lims;titlelabel="planview plot") where T <: Real
-
-#     cplan = planview(c::Field{T},depth)
-
-#     cmap_seismic = get_cmap("seismic")
-    
-#     #calc fignum - based on current number of figures
-#     figure()
-#     contourf(c.γ.lon,c.γ.lat, cplan', lims, cmap=cmap_seismic)
-#     #fig, ax = plt.subplots()
-#     CS = gca().contour(c.γ.lon,c.γ.lat, cplan', lims, cmap=cmap_seismic)
-#     gca().clabel(CS, CS.levels, inline=true, fontsize=10)
-#     ylabel("Latitude [°N]")
-#     xlabel("Longitude [°E]")
-#     gca().set_title(titlelabel)
-#     colorbar(orientation="vertical")
-    
-# end
-
-# """
-#     function planviewplot
-#     Plot of plan view (lon-lat) in ocean
-# # Arguments
-# - `field::BoundaryCondition`, 3d filed of values to be plotted
-# - `depth`: depth of plan view
-# - `lims`: contour levels
-# - `γ::Grid`, needed for lat, lon but not in BoundaryCondition! (could refactor)
-# - `titlelabel`: optional title label
-# """
-# function planviewplot(b::BoundaryCondition{T}, lims,γ::Grid;titlelabel="surface plot") where T <: Real
-
-#     # is the boundary condition oriented correctly?
-#     if b.dim != 3
-#         error("boundary condition not horizontal")
-#     end
-    
-#     cplan = b.tracer
-    
-#     cmap_seismic = get_cmap("seismic")
-    
-#     #calc fignum - based on current number of figures
-#     figure()
-#     contourf(γ.lon,γ.lat, cplan', lims, cmap=cmap_seismic)
-#     #fig, ax = plt.subplots()
-#     CS = gca().contour(γ.lon,γ.lat, cplan', lims, cmap=cmap_seismic)
-#     gca().clabel(CS, CS.levels, inline=true, fontsize=10)
-#     ylabel("Latitude [°N]")
-#     xlabel("Longitude [°E]")
-#     gca().set_title(titlelabel)
-#     colorbar(orientation="vertical")
-    
-# end
 
 end
